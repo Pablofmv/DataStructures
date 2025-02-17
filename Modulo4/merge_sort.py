329261707598b7b9bdc7fdda9643f1627afe1cd0
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-02-17 12:54:02 -0500

    new files data structure

--- Modulo4/merge_sort.py
+++ Modulo4/merge_sort.py
@@ -26,6 +26,44 @@ def merge_sort(arr):
 
     return merge(left_half, right_half)
 
+
+def nerge_a(left_part, right_part):
+    
+    sorted_array = []
+    i,j = 0,0
+
+    while i < len(left_part) and j < len(right_part):
+        if left_part[i] < right_part[j]:
+            sorted_array.append(left_part[i])
+            i += 1
+        else:
+            sorted_array.append(right_part[j])
+            j += 1
+        
+    
+    sorted_array.extend(left_part[i:])
+    sorted_array.extend(right_part[j:])
+
+    return sorted_array
+
+
+
+def merge_sort_a(arr):
+    if len(arr) == 1:
+        return arr
+   
+    n = len(arr)
+    mid = n // 2
+
+    left_part = merge_sort_a(arr[0:mid])
+    right_part = merge_sort_a(arr[mid:])
+    
+    return nerge_a(left_part,right_part)
+
+
+arr = [7,2,9,4,6,1]
+print(merge_sort_a(arr))
+
 arr = [7,2,9,4,6,1]
 print(merge_sort(arr))
 
