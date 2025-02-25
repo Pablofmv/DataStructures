3a4c77cc07af389c4998d5bbcbfa7e3639856311
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-02-25 18:42:30 -0500

    Added new files to DataStructures

---
+++ Modulo4/counting_sort.py
@@ -0,0 +1,27 @@
+def counting_sort(arr):
+
+    M = max(arr)
+
+    Count = [0] * (M + 1)
+
+    for num in arr:
+        Count[num] += 1
+    
+    Pos = [0] * (M + 1)
+    Pos[1] = 1
+
+    for j in range(2,M + 1):
+        Pos[j] = Pos[j - 1] + Count[j - 1]
+
+    sorted_array = [0] * len(arr)
+
+    for num in arr:
+        sorted_array[Pos[num] - 1] = num
+        Pos[num] += 1
+    
+    return sorted_array
+
+
+
+A = [4,2,2,8,3,3,1] 
+print(counting_sort(A))
