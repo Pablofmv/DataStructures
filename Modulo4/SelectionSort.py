4903fed9771fcf1110fc4258829233211eca823c
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-02-26 18:25:58 -0500

    Added new files to DataStructures

--- Modulo4/SelectionSort.py
+++ Modulo4/SelectionSort.py
@@ -11,5 +11,36 @@ def SelectionSort(A):
     
     return A
 
+def selection_sort(arr):
 
+    n = len(arr)
+
+    for i in range(0,n):
+        min_index = i
+        for j in range(i+1,n):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index] , arr[i]
+    
+    return arr
+
+def select_sort(arr):
+
+    n = len(arr)
+
+    for i in range(0,n):
+        min_index = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
+
+
+
+print(select_sort([7,6,5,4,3,2,1,8]))
+print(selection_sort([7,6,5,4,3,2,1,8]))
 print(SelectionSort([7,6,5,4,3,2,1,8]))
