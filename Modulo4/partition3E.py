1cadaa574a6c316bfd32e627f10e61e310ec3674
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-07 16:24:43 -0400

    Added some files DataStructures

---
+++ Modulo4/partition3E.py
@@ -0,0 +1,46 @@
+import random
+
+def partition3(A,l,r):
+
+    x = A[l]
+
+    m1 = l
+    m2 = l
+
+    for i in range(l+1,r+1):
+
+        if A[i] < x:
+            m1 += 1
+            m2 += 1
+
+            A[i], A[m1] = A[m1], A[i]
+
+            if m1 != m2:
+                A[i], A[m2] = A[m2], A[i]
+
+        elif A[i] == x:
+            m2 += 1
+            A[i], A[m2] = A[m2], A[i]
+    
+    A[l], A[m1] = A[m1], A[l]
+
+    return m1, m2
+
+def random_quick_sort(A,l,r):
+
+    if l >= r:
+        return 
+    
+    k = random.randint(l,r)
+    A[l],A[k] = A[k],A[l]
+    
+    m1, m2 = partition3(A,l,r)
+
+    random_quick_sort(A,l,m1-1)
+    random_quick_sort(A,m2+1,r)
+
+    return A
+
+A = [2,2,1,2]
+
+print(random_quick_sort(A,0,len(A)-1))
