1b1a639e28a833599f8a12009d5ffbb00c42b7d3
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-11 17:40:42 -0400

    Added files to DataStructures

--- Modulo4/Partition3G.py
+++ Modulo4/Partition3G.py
@@ -1,6 +1,7 @@
 import random
 
 def partition3(A,l,r):
+
     x = A[l]
     m1 = l
     m2 = l
@@ -10,31 +11,32 @@ def partition3(A,l,r):
             m1 += 1
             m2 += 1
             A[i], A[m1] = A[m1], A[i]
+
             if m1 != m2:
-                A[i], A[m2] = A[m2], A[i]
+                A[i] , A[m2] = A[m2], A[i]
+
         elif A[i] == x:
             m2 += 1
-            A[i],A[m2] = A[m2],A[i]
-    
+            A[i], A[m2] = A[m2], A[i]
+        
     A[l], A[m1] = A[m1], A[l]
-
+    
     return m1, m2
 
-def random_quick_sort3(A,l,r):
+def random_quick_sort(A,l,r):
 
     if l >= r:
-        return
+        return 
     
     k = random.randint(l,r)
-
-    A[l],A[k] = A[k], A[l]
-
+    A[l], A[k] = A[k], A[l]
+    
     m1, m2 = partition3(A,l,r)
 
-    random_quick_sort3(A,l,m1-1)
-    random_quick_sort3(A,m2+1,r)
+    random_quick_sort(A,l,m1-1)
+    random_quick_sort(A,m2+1,r)
 
     return A
 
 A = [4,3,2,1]
-print(random_quick_sort3(A,0,len(A)-1))
+print(random_quick_sort(A,0,len(A) - 1))
