aad6e8bc05dce39b6ece8620de7d19da1e53bc9e
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-12 14:30:25 -0400

    Added files to DataStructures

--- Modulo4/Programming Assignment/BinarySearchWithDuplicates.py
+++ Modulo4/Programming Assignment/BinarySearchWithDuplicates.py
@@ -133,9 +133,35 @@ def binary_search_duplicates7(A,key):
         return -1
     
     return binary_search_duplicates8(A,0,len(A)-1,key)
+
+def binary_search_duplicates9(A,key):
+    def binary_search_duplicates10(A,low,high,key):
+
+        if high < low:
+            return -1
+
+        mid = low + (high -low) // 2
+
+        if A[mid] == key:
+
+            if mid == 0 or A[mid -1] < key:
+                return mid
+            else:
+                return binary_search_duplicates10(A,low,mid-1,key)
+        elif key < A[mid]:
+            return binary_search_duplicates10(A,low,mid-1,key)
+        else:
+            return binary_search_duplicates10(A,mid+1,high,key)
+    
+    if not A:
+        return -1
+    
+    return binary_search_duplicates10(A,0,len(A)-1,key)
+
+
     
     
 A = [1,3,3,5,7]
-print(binary_search_duplicates7(A,3))
+print(binary_search_duplicates9(A,5))
 
     
