a0aad2a0e0e14d5415b6e09a17248afc5de32e47
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-17 19:48:06 -0400

    Added files to DataStructures

---
+++ Modulo5/DpChange2.py
@@ -0,0 +1,29 @@
+import time
+
+def dp_change(money, coins):
+    min_num_coins = [float("inf")] * (money + 1)
+    min_num_coins[0] = 0
+
+    for m in range(1,money+1):
+        for coin in coins:
+            if coin <= m:
+                num_coins = min_num_coins[m-coin] + 1
+                if num_coins <= min_num_coins[m]:
+                    min_num_coins[m] = num_coins
+    
+    if min_num_coins[money] == float("inf"):
+        return -1
+    
+    return min_num_coins[money]
+
+money = 6
+coins = [1,3,4]
+
+start_time = time.time()
+print(dp_change(money,coins))
+end_time = time.time()
+
+print(f"{end_time - start_time:.6f}")
+
+
+
