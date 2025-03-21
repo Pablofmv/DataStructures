8519713bc8ad6b285afc00fa4ca4ac3854cbeef2
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-21 10:14:43 -0400

    Added files to DataStructures

---
+++ Modulo5/EditDistance3.py
@@ -0,0 +1,23 @@
+def edit_distance(A,B):
+
+    n = len(A)
+    m = len(B)
+
+    if n == 0:
+        return m
+    if m ==0:
+        return n
+    
+    if A[n-1] == B[m-1]:
+        return edit_distance(A[:n-1],B[:m-1])
+    
+    insert = 1 + edit_distance(A,B[:m-1])
+    delete = 1 + edit_distance(A[:n-1],B)
+    substitution = 1 + edit_distance(A[:n-1],B[:m-1])
+
+    return min(insert,delete,substitution)
+
+A = "Pabl"
+B = "Pablo"
+
+print(edit_distance(A,B))
