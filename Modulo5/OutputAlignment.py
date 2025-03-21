25282b277cbbcdaa20cc3bacf43280781c232b01
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-21 17:36:25 -0400

    Added files to DataStructures

---
+++ Modulo5/OutputAlignment.py
@@ -0,0 +1,29 @@
+def edit_distance(A,B):
+    n = len(A)
+    m = len(B)
+
+    D = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+    for i in range(0,n+1):
+        D[i][0] = i
+
+    for j in range(0,m+1):
+        D[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            insertion = D[i][j-1] + 1
+            deletion = D[i-1][j] + 1
+            if A[i-1] == B[j-1]:
+                match = D[i-1][j-1]
+            else:
+                match = D[i-1][j-1] + 1
+
+            D[i][j] = min(insertion, deletion,match)
+    
+    return D
+
+A = 'kitten'
+B = 'sitting'
+
+print(edit_distance(A,B))
