78a57f5516c215eb15db79cb5379c1d070692041
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-28 10:26:53 -0400

    added files to DataStructures

--- Modulo5/PrimitiveCalculator2.py
+++ Modulo5/PrimitiveCalculator2.py
@@ -1,30 +1,31 @@
 def primitive_calculator(amount):
-
+    
     min_operations = [float("inf")] * (amount + 1)
     min_operations[1] = 0
-    prev  = [0] * (amount + 1)
+    prev = [0] * (amount + 1)
 
     for m in range(2,amount+1):
 
-        add_one = min_operations[m-1] + 1
-        rest_operations = add_one
+        add_1 = min_operations[m-1] + 1
+        rest_operations = add_1
         prev[m] = m - 1
 
-        if m%2 == 0:
-            divide_2 = min_operations[m//2] + 1
+        if m % 2 == 0:
+            divide_2 = min_operations[m // 2] + 1
+
             if divide_2 < rest_operations:
                 rest_operations = divide_2
                 prev[m] = m // 2
-        
-        if m%3 == 0:
-            divide_3 = min_operations[m//3] + 1
+
+        if m % 3 == 0:
+            divide_3 = min_operations[m // 3] + 1
+
             if divide_3 < rest_operations:
                 rest_operations = divide_3
                 prev[m] = m // 3
         
         min_operations[m] = rest_operations
-
-
+    
     if min_operations[amount] == float("inf"):
         return -1, []
     
@@ -33,12 +34,13 @@ def primitive_calculator(amount):
 
     while current != 0:
         sequence.append(current)
-        current = prev[current]
-    
+        current = prev[current]  
+
     sequence.reverse()
     
-    return min_operations[amount], sequence
+    return min_operations[amount],sequence
 
-amount =96234
+amount = 96234
 print(primitive_calculator(amount))
+
 
