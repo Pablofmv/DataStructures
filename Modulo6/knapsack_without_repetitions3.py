5d1fa0cc4bc3c8472a6afc7689816ca078c0f693
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-03-30 21:15:32 -0400

    Added files to DataStructures

---
+++ Modulo6/knapsack_without_repetitions3.py
@@ -0,0 +1,20 @@
+def knapsack_wo_repetitions(W,weights,values):
+
+    n = len(weights)
+
+    dp = [[0 for _ in range(0,n+1)] for _ in range(0,W+1)]
+
+    for i in range(1,n+1):
+        for w in range(1,W+1):
+
+            dp[w][i] = dp[w][i-1]
+
+            if weights[i - 1] <= w:
+                dp[w][i] = max(dp[w][i], dp[w - weights[i-1]][i-1]+values[i-1])
+
+    return dp[W][n]
+
+W = 3
+weights = [1, 2]
+values = [2, 3]
+print(knapsack_wo_repetitions(W,weights,values))
