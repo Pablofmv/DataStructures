398a73f184e7006a094c29f76dc02c9b4001aa37
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-04-03 12:33:06 -0400

    Added files to DataStructures

--- Modulo6/maximize_expression2.py
+++ Modulo6/maximize_expression2.py
@@ -8,8 +8,8 @@ def apply_op(a, b, op):
         return a * b
     else:
         return "Error"
-    
-def MinAndMax(i,j,d,ops,m,M):
+
+def min_and_max(i, j, d, ops, m, M):
 
     if i == j:
         return d[i], d[i]
@@ -17,46 +17,44 @@ def MinAndMax(i,j,d,ops,m,M):
     min_val = float("inf")
     max_val = float("-inf")
 
-
     for k in range(i,j):
-        left_min, left_max = m[i][k], M[i][k]
 
+        left_min, left_max = m[i][k], M[i][k]
         right_min, right_max = m[k+1][j], M[k+1][j]
 
         op = ops[k]
 
         a = apply_op(left_max, right_max, op)
-        b = apply_op(left_max, right_min, op)
-        c = apply_op(left_min, right_max, op)
-        d = apply_op(left_min, right_min, op)
+        b = apply_op(left_min, right_max, op)
+        c = apply_op(left_min, right_min, op)
+        d = apply_op(left_max, right_min, op)
 
         min_val = min(min_val, a, b, c, d)
         max_val = max(max_val, a, b, c, d)
-
+    
     return min_val, max_val
-
+    
 def maximize_expression(d, ops):
 
     n = len(d)
 
     if n != len(ops) + 1:
-        return "Number of operators must be one less than number of numbers."
+        return "Error"
     
-    m = [[0 for _ in range(0, n)] for _ in range(0,n)]
-    M = [[0 for _ in range(0, n)] for _ in range(0,n)]
+    m = [[0 for _ in range(0, n)] for _ in range(0, n)]
+    M = [[0 for _ in range(0, n)] for _ in range(0, n)]
 
-    for i in range(0, n):
+    for i in range(0,n):
         m[i][i] = d[i]
         M[i][i] = d[i]
     
     for s in range(1,n):
         for i in range(0,n-s):
             j = i + s
-            m[i][j], M[i][j] = MinAndMax(i, j, d, ops, m ,M)    
+            m[i][j], M[i][j] = min_and_max(i, j, d, ops, m, M)
     
     return M[0][n-1]
 
-d = [5,8,7,4,8,9]
-ops = ['-','+','x','-','+']
+d = [2 , 3, 4]
+ops = ['+','x']
 print(maximize_expression(d, ops))
-    
